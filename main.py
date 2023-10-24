b190e79435a76d37d45da2ccb9050c20405cbdf7
Wen-Hao Wu <vance.wenhaowu@gmail.com>
2023-10-24 14:33:06 +0200

    create detection page, training page, and 2 buttons to go to repective pages

--- main.py
+++ main.py
@@ -6,8 +6,16 @@ ssl._create_default_https_context = ssl._create_unverified_context()
 app = Flask(__name__)
 
 @app.route("/")
-def homepage():
+def home_page():
     return render_template('home.html')
+
+@app.route("/training")
+def training_page():
+    return render_template('training.html')
+
+@app.route("/detection")
+def detection_page():
+    return render_template('detection.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=1008, debug=True)
