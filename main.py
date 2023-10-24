198f75ab2d7323feabbabdda47b6d95ebede1b5c
Wen-Hao Wu <vance.wenhaowu@gmail.com>
2023-10-24 15:14:21 +0200

    added documentation

--- main.py
+++ main.py
@@ -1,20 +1,34 @@
+"""
+This file contains scripts to create a web application using flask
+"""
+
 from flask import Flask, render_template, request, redirect, url_for
 import ssl
 
+# by-passing the ssl verification
 ssl._create_default_https_context = ssl._create_unverified_context()
 
 app = Flask(__name__)
 
 @app.route("/")
 def home_page():
+    """
+    This is a function to render home page
+    """
     return render_template('home.html')
 
 @app.route("/training")
 def training_page():
+    """
+    This is a function to render training page
+    """
     return render_template('training.html')
 
 @app.route("/detection")
 def detection_page():
+    """
+    This is a function to render detection page
+    """
     return render_template('detection.html')
 
 if __name__ == '__main__':
